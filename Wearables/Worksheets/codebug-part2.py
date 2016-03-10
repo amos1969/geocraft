c20c3fc6f6c293abe044722188d8f5e43d0e02ee
amos1969 <amos@amos-site.org.uk>
2016-03-10 21:07:30 +0000

    Minor edits to the formatting and content

--- Wearables/Worksheets/codebug-part2.py
+++ Wearables/Worksheets/codebug-part2.py
@@ -7,11 +7,11 @@ codebug.set_leg_io(0, 1)
 codebug.set_pullup(0, 1)
 codebug.clear()
 
-message = codebug_tether.sprites.StringSprite("Hello MOSI")
+message = codebug_tether.sprites.StringSprite("Hello MSI")
 
 while True:
     if codebug.get_input(0) == 0:
-        for i in range (50):  
+        for i in range(50):  
             codebug.draw_sprite(-i, 0, message)
             time.sleep(0.05)      
     else:
--- Wearables/Worksheets/codebug-part3.py
+++ Wearables/Worksheets/codebug-part3.py
@@ -13,14 +13,12 @@ codebug.clear()
 
 mc = Minecraft.create()
 
-message = codebug_tether.sprites.StringSprite("Hello MOSI")
-
 while True:
     pos = mc.player.getPos()
     if codebug.get_input(0) == 0:
-        mc.setBlock(pos.x, pos.y, pos.z, block.TNT.id, 1)
+        mc.setBlock(pos.x, pos.y-1, pos.z, block.TNT.id, 1)
     elif codebug.get_input(1) == 0:
-        mc.setBlock(pos.x, pos.y, pos.z, block.LAVA.id)        
+        mc.setBlock(pos.x, pos.y-1, pos.z, block.LAVA.id)        
     else:
         codebug.clear()
 
