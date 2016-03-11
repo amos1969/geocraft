084a5159d50e3baaa85b7c37d84cd32ce8c31d2f
amos1969 <amos@amos-site.org.uk>
2016-03-11 21:24:09 +0000

    Changed second code snippet so that it matches the first

--- Wearables/Worksheets/trails2.py
+++ Wearables/Worksheets/trails2.py
@@ -4,11 +4,8 @@ import random
 mc = Minecraft.create()
 while True:
     pos = mc.player.getPos()
-    x = pos.x
-    y = pos.y
-    z = pos.z
     which_brick = random.randint(0, 3)
-    mc.setBlock(x, y-1, z, block.STONE_BRICK.id, which_brick)
+    mc.setBlock(pos.x, pos.y-1, pos.z, block.STONE_BRICK.id, which_brick)
 
 
 
