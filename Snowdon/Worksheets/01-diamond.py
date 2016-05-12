63aec3c51c83aae31bf22feeafe3b64dc5d10aed
amos1969 <amos@amos-site.org.uk>
2016-05-12 11:40:25 +0100

    Modified code and associated worksheet for lesson 1

--- Snowdon/Worksheets/01-diamond.py
+++ Snowdon/Worksheets/01-diamond.py
@@ -1,8 +1,6 @@
 from mcpi.minecraft import Minecraft
-import mcpi.block as block
+from mcpi import block
 mc = Minecraft.create()
+
 pos = mc.player.getPos()
-x = pos.x
-y = pos.y
-z = pos.z
-mc.setBlock(x, y, z, block.GOLD_BLOCK.id)
+mc.setBlock(pos.x, pos.y, pos.z, block.GOLD_BLOCK.id)
--- Snowdon/Worksheets/02-glass.py
+++ Snowdon/Worksheets/02-glass.py
@@ -1,11 +1,8 @@
 from mcpi.minecraft import Minecraft
-import mcpi.block as block
+from mcpi import block
 mc = Minecraft.create()
+
 pos = mc.player.getPos()
-x = pos.x + 1
-y = pos.y
-z = pos.z + 1
-x1 = pos.x + 5
-y1 = pos.y + 5
-z1 = pos.z + 5
-mc.setBlocks(x, y, z, x1, y1, z1, block.GLASS.id)
+mc.setBlocks(pos.x+1, pos.y, pos.z+1,
+             pos.x+5, pos.y+5, pos.z+5,
+             block.GLASS.id)
